268b3793529f6f036cf03b828c78e25f5fe91ab4
salhernandez <hernandezg.sal@gmail.com>
2017-09-10 21:41:08 -0700

    cycling through all tags

--- payload.js
+++ payload.js
@@ -1,2 +1,13 @@
 // send the page title as a chrome message
 chrome.runtime.sendMessage(document.title);
+console.log(document);
+
+//get all the tags
+var all = document.getElementsByTagName("*");
+// console.log(all);
+
+//cycle through all tags
+for (var i=0, max=all.length; i < max; i++) {
+    // Do something with the element here
+    console.log(all[i]);
+}
