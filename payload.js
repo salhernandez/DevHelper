cfe4120ca86afe368d9545c0f9479b6cbcea1f1c
salhernandez <hernandezg.sal@gmail.com>
2017-09-10 22:38:42 -0700

    checks href for stackoflow.com and not the chached link

--- payload.js
+++ payload.js
@@ -11,5 +11,9 @@ for (var i=0, max=all.length; i < max; i++) {
     // Do something with the element here
     // console.log(all[i]);
 
-    console.log(all[i].getAttribute("href"));
+    // console.log(all[i].getAttribute("href"));
+    var a = String(all[i].getAttribute("href"));
+    if(a.includes("stackoverflow.com") && !a.includes("webcache")){
+        console.log(a);
+    }
 }
