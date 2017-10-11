055f9cb62e717afb3e155519da1064cee5a0db7c
salhernandez <hernandezg.sal@gmail.com>
2017-10-10 21:35:09 -0700

    removed unecessary semicolon

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function (evt) {
 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
 		file: 'payload.js'
-	});;
+	});
 });
 
 // Listen to messages from the payload.js script and write to popout.html
