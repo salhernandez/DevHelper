4ddcc81f763360847e90c42b79eea07ab535deb5
salhernandez <hernandezg.sal@gmail.com>
2017-10-10 22:02:00 -0700

    added ajax call

--- payload.js
+++ payload.js
@@ -25,3 +25,24 @@ for (var i=0, max=all.length; i < max; i++) {
         console.log(a);
     }
 }
+
+//makes request
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+
+var request = makeHttpObject();
+request.open("GET", "https://www.google.com/", true);
+request.send();
+request.onreadystatechange = function() {
+  if (request.readyState == 4){
+    console.log(request.responseText);
+  }
+};
