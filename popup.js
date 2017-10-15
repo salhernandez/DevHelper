a2ebad0286dd134362f5b2e0c5f927618c81a12b
salhernandez <hernandezg.sal@gmail.com>
2017-10-15 11:10:34 -0700

    can obtain current tab url

--- popup.js
+++ popup.js
@@ -7,6 +7,7 @@
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
+		console.log(tab.url);
 		chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
 			file: 'payload.js'
 		});
