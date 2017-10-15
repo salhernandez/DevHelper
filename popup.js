3a817778a10ad665b956f19f96deb99d3985826b
salhernandez <hernandezg.sal@gmail.com>
2017-10-15 11:09:41 -0700

    script now runs in the background

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 // Inject the payload.js script into the current tab after the popout has loaded
-window.addEventListener('load', function (evt) {
-	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-		file: 'payload.js'
-	});
-});
+// window.addEventListener('load', function (evt) {
+// 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+// 		file: 'payload.js'
+// 	});
+// });
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.active) {
+		chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+			file: 'payload.js'
+		});
+  }
+})
 
 // Listen to messages from the payload.js script and write to popout.html
 chrome.runtime.onMessage.addListener(function (message) {
