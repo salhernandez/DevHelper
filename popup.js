3a33f92f0a09f615302153ed3c9177a6490d9e16
salhernandez <hernandezg.sal@gmail.com>
2017-10-19 21:57:52 -0700

    added basics of lsiteners

--- popup.js
+++ popup.js
@@ -27,3 +27,10 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 		}
   }
 })
+
+var a=0;
+function count() {
+    a++;
+    document.getElementById('demo').textContent = a;
+}
+document.getElementById('do-count').onclick = count;
